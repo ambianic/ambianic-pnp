1628d4af750799df625b90cdce632843b5bf519a
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-07-30 01:15:03 +0100

    feat: include errorHandler middleware for handling errors

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const publicContent = require('../../app.json');
 
-module.exports = ({ config, realm, messageHandler }) => {
-  const authMiddleware = require('./middleware/auth')({ config, realm });
+module.exports = ({
+  config,
+  realm,
+  messageHandler
+}) => {
+  const authMiddleware = require('./middleware/auth')({
+    config,
+    realm
+  });
 
   const app = express.Router();
 
@@ -16,10 +23,29 @@ module.exports = ({ config, realm, messageHandler }) => {
     res.send(publicContent);
   });
 
-  app.use('/:key', require('./v1/public')({ config, realm }));
-  app.use('/:key/:id/:token', authMiddleware , jsonParser, require('./v1/calls')({ realm, messageHandler }));
+  /*
+   * Handle 404 error
+   */
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      errors: {
+        msg: 'URL_NOT_FOUND'
+      }
+    })
+  })
+
+  app.use('/:key', require('./v1/public')({
+    config,
+    realm
+  }));
+  app.use('/:key/:id/:token', authMiddleware, jsonParser, require('./v1/calls')({
+    realm,
+    messageHandler
+  }));
   // console.log('realm', realm)
-  app.use('/:key/:id/:token/room', authMiddleware, jsonParser, require('./v1/rooms')({ realm }));
+  app.use('/:key/:id/:token/room', authMiddleware, jsonParser, require('./v1/rooms')({
+    realm
+  }));
 
   return app;
 };
