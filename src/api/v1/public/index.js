940e630efdaa5dc64dc09ac2570f9f4d9e0a56c4
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-07-30 11:49:07 +0100

    push codes to github

--- src/api/v1/public/index.js
+++ src/api/v1/public/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
-module.exports = ({ config, realm }) => {
+module.exports = ({
+  config,
+  realm
+}) => {
   const app = express.Router();
 
   // Retrieve guaranteed random ID.
